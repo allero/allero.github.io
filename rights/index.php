973ab6a92ca477a724de4e0015dee462348ceb3a
Rodney Allen <rodneyrocafella@gmail.com>
2018-05-08 23:35:06 +0900

    aaaaa

--- rights/index.php
+++ rights/index.php
@@ -38,7 +38,7 @@ if(isset($_POST['submitted'])) {
 	// upon no failure errors let's email now!
 	if(!isset($hasError)) {
 		
-		$emailTo = 'youremail@email.com'; // ADD YOUR EMAIL ADDRESS HERE FOR CONTACT FORM!
+		$emailTo = 'abrighterstartchildcare@gmail.com'; // ADD YOUR EMAIL ADDRESS HERE FOR CONTACT FORM!
 		$subject = 'Submitted message from '.$name; // ADD YOUR EMAIL SUBJECT LINE HERE FOR CONTACT FORM!
 		$sendCopy = trim($_POST['sendCopy']);
 		$body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
@@ -57,12 +57,12 @@ if(isset($_POST['submitted'])) {
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>The Parallax Template</title>
+		<title>A BRIGHTER START CHILDCARE</title>
 		<link href="css/jquery.bxslider.css" rel="stylesheet" />
 		<link href="style.css" rel="stylesheet">
 		<link href="css/font-awesome.min.css" rel="stylesheet"/>
-		<link rel="shortcut icon" type="image/png" href="img/w3-favicon.png"/>
-		<meta name="description" content="">
+		<link rel="shortcut icon" type="image/png" href="logo.png"/>
+		<meta name="description" content="A Brighter Start Childcare is an educational based childcare center. Dedicated to educating children and preparing them for a bright future.">
 	</head>
 <body>
 	<a name="home">
@@ -72,11 +72,11 @@ if(isset($_POST['submitted'])) {
 		<div class="nav-outer">
 		<div class="nav-wrap">
 			<nav class="navigation">
-				<div class="logo"><img src="img/logo.png" alt="Newbie"></div>
+				<div class="logo"><img src="logo.png" alt="Newbie"></div>
 				<div class='nav' nav-menu-style="yoga">
 					<ul class="nav-menu">
 						<li><a href="#home">Home</a></li>
-						<li><a href="#about">About Us</a></li>
+						<li><a href="#about" class="transition">Mission</a></li>
 						<li><a href="#services">Services</a></li>
 						<li><a href="#testimonials">Testimonials</a></li> 
 						<li><a href="#contact">Contact</a></li>
@@ -93,10 +93,10 @@ if(isset($_POST['submitted'])) {
 		<div class="slide-wrap">
 			<section class="slider">
 				<ul class="slider1">
-					<li><img src="https://www.w3newbie.com/wp-content/uploads/slider-image-3.png" title="" /></li>
-					<li><img src="https://www.w3newbie.com/wp-content/uploads/slider-image-1.png" title="" /></li>
-					<li><img src="https://www.w3newbie.com/wp-content/uploads/slider-image-2.png" title="" /></li>
-					<li><img src="https://www.w3newbie.com/wp-content/uploads/slider-image-4.png" title="" /></li>
+					<li><img src="img/Daycare1.jpg" title="" /></li>
+					<li><img src="img/duckduck.jpg" title="" /></li>
+					<li><img src="img/IMG_1855.jpg" title="" /></li>
+					<li><img src="img/slider-2000x800.png" title="" /></li>
 				</ul>
 			</section>
 		</div>
@@ -128,14 +128,14 @@ if(isset($_POST['submitted'])) {
 <!--- Sart Banner Wrapper -->
 	<a name="about">
 	<div id="banner-wrapper">
-		<h1>Welcome To Our Website</h1>
+		<h1>A Brighter Start Childcare</h1>
 			<div class="welcome-column">
 				<div class="welcome-icon">
 					<i class="fa fa-keyboard-o"></i>
 				</div>
 				<div class="welcome-text">
-					<h3>heading title section</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					<h3>Mission Statement</h3>
+					<p>At A Brighter Start, we believe in building a strong foundation for each child to grow emotionally, socially, and cognitively at his/her own pace.</p>
 				</div>
 			</div>
 			<div class="welcome-column">
@@ -143,8 +143,8 @@ if(isset($_POST['submitted'])) {
 					<i class="fa fa-laptop"></i>
 				</div>
 				<div class="welcome-text">
-					<h3>heading title section</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					<h3>Teaching Environment</h3>
+					<p>In our fun and exciting environment we believe that children deserves to receive all the love, support and the respect it takes to establish a strong foundation for their future success.</p>
 				</div>
 			</div>
 	</div>
@@ -157,17 +157,17 @@ if(isset($_POST['submitted'])) {
 <!--- Start Heading & Text Section -->
 		<section class="one-third">
 			<td><i class="fa fa-youtube-play"></i></td>
-			<h3>Heading title</h3>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+			<h3>Assesments</h3>
+			<p>We provide routine assessments for compliance of procedures, curriculum, general happiness and the well being of each child.</p>
 		</section>
 		<section class="one-third">
 			<td><i class="fa fa-html5"></i></td>
-			<h3>Heading title</h3>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>		</section>
+			<h3>Bright Children</h3>
+			<p>Our program has a wide variety of experiences to help children learn about themselves and their world around them. </p>		</section>
 		<section class="one-third">
 			<td><i class="fa fa-desktop"></i></td>
-			<h3>heading title</h3>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>		</section>
+			<h3>Bright Curriculum</h3>
+			<p>Our curriculum values the need to engage the student in sustained meaningful encounters.</p>		</section>
 <!--- End Heading & Text Section -->
 	</div>
 </section>
@@ -177,15 +177,15 @@ if(isset($_POST['submitted'])) {
 <!--- Start Heading & Text Sections -->
 	<a name="services">
 		<section class="left-col">
-			<h1>Heading title Text</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+			<h1>Our Program</h1>
+			<p>Our program includes experience such as computers, math, language arts, music, drama, physical fitness, dance, motor skills, group play, social-dramatic play, crafts, science, kitchen/home experience as well as our exciting early reading program. </p>
+			<p>Our curriculum shows that learning through play produces a vigorous cognitive growth that focus on a combination of linguistic and logical intelligence. </p>
+			<p>We promote an open door policy so communication with parents is always welcomed. We focus on quality assurance.</p>
 		</section>
 <!--- End Heading & Text Sections -->
 <!--- Start Shedule & Contact Sidebar -->
 		<section class="sidebar">
-			<img src="https://www.w3newbie.com/wp-content/uploads/beach-building.jpg" title=""/>				
+			<img src="img/sidebar-image-500x747.jpg" title=""/>				
 		</section>
 <!--- End Shedule & Contact Sidebar -->
 			<div class="welcome-column">
@@ -193,8 +193,8 @@ if(isset($_POST['submitted'])) {
 					<i class="fa fa-keyboard-o"></i>
 				</div>
 				<div class="welcome-text">
-					<h3>heading title section</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					<h3>Birthdays</h3>
+					<p>Beginning 2015, each child’s birthday is his/her “special day.” Unfortunately, we request that children save the cake and ice cream for home.</p>
 				</div>
 			</div>
 			<div class="welcome-column">
@@ -202,24 +202,24 @@ if(isset($_POST['submitted'])) {
 					<i class="fa fa-laptop"></i>
 				</div>
 				<div class="welcome-text">
-					<h3>heading title section</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					<h3>Commitment</h3>
+					<p>As an individual who works with young children, we commit myself to furthering the values of early childhood education as they are reflected in the ideals and principles of NAEYC Code of Ethical Conduct. </p>
 				</div>
 			</div>
 	<div class="clearfix"></div>
 <!--- Start Four Column Pictures & Text -->
 	<a name="testimonials">
 		<section class="one-fourth">
-			<img src="https://www.w3newbie.com/wp-content/uploads/lemons.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 		</section>
 		<section class="one-fourth">
-			<img src="https://www.w3newbie.com/wp-content/uploads/flower.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 		</section>
 		<section class="one-fourth">
-			<img src="https://www.w3newbie.com/wp-content/uploads/watermelon.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 		</section>
 		<section class="one-fourth">
-			<img src="https://www.w3newbie.com/wp-content/uploads/coffee.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 		</section>
 <!--- End Four Column Pictures & Text -->
 	</div>
@@ -231,16 +231,16 @@ if(isset($_POST['submitted'])) {
 <!--- Start Heading & Text Section -->
 		<section class="one-third">
 			<td><i class="fa fa-laptop"></i></td>
-			<h3>Heading title</h3>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+			<h3>1st Peson Nice Words</h3>
+			<p>Some of our preschoolers are reading on a kindergarten level.</p>
 		</section>
 		<section class="one-third">
 			<td><i class="fa fa-camera-retro"></i></td>
-			<h3>Heading title</h3>
+			<h3>Testimony 2</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>		</section>
 		<section class="one-third">
 			<td><i class="fa fa-keyboard-o"></i></td>
-			<h3>heading title</h3>
+			<h3>Testimony 3</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>		</section>
 <!--- End Heading & Text Section -->
 	</div>
@@ -249,16 +249,18 @@ if(isset($_POST['submitted'])) {
 	<div id="banner-wrapper">
 	<div class="clearfix"></div>
 <!--- Start Image Section -->
+		<div class="review">
 			<section class="left-col">
-				<img src="https://www.w3newbie.com/wp-content/uploads/beach-surf.jpg" title=""/>
+				<img src="img/740x370.jpg" title=""/>
 			</section>
 <!--- End Image Section -->
 <!--- Start Heading & Text -->
 			<section class="sidebar">
-				<h3>Heading Title Text</h3>
+				<h3>Heading Title Text</h4>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 			</section>
+		</div>
 <!--- End Heading & Text -->
 	<div class="clearfix"></div>
 <!--- Start Heading & Text Section -->
@@ -267,23 +269,24 @@ if(isset($_POST['submitted'])) {
 	<div class="clearfix"></div>
 <!--- Start Image, Title & Text -->
 		<section class="one-third">
-			<img src="https://www.w3newbie.com/wp-content/uploads/beach-camera.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 			<h3>Heading title text</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 		</section>
 		<section class="one-third">
-			<img src="https://www.w3newbie.com/wp-content/uploads/girl-camera.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 			<h3>heading title text</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 		</section>
 		<section class="one-third">
-			<img src="https://www.w3newbie.com/wp-content/uploads/girl-beach.png" title=""/>
+			<img src="img/500x371.png" title=""/>
 			<h3>Heading title text</h3>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 		</section>
 <!--- End Image, Title & Text -->
-	</div>
 	<div class="clearfix"></div>
+		</div>
+<!--- End Banner Wrapper -->
 <!--- Start Parallax Section -->
 <section class="parallax-3">
 	<div class="parallax-inner2">
@@ -388,7 +391,7 @@ if(isset($_POST['submitted'])) {
 <!--- End Banner Wrapper -->			
 <!--- Start Footer -->
 		<footer>
-			<p>&copy; The Parallax Template, 2017.</p>
+			<p>&copy; Rodney Allen, 2018.</p>
 		</footer>
 <!--- End Footer -->
 <!--- Top Scroll Start -->
